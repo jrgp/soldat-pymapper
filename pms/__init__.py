cad9ae55d8ee2b91b4f5bd633ccab110857cb2ba
Joe Gillotti <joe@u13.net>
2016-02-15 18:30:06 -0800

    Tweaks to PMS reader
    - Add checks to not proceed if readinto returns 0 in any case. Some
      maps seem to have a falsly huge waypoint count which resulted in an
      infinite loop before I added this check
    
    - Add some standard python logging

--- pms/__init__.py
+++ pms/__init__.py
@@ -8,11 +8,13 @@ from pms.collider import T_Collider
 from pms.spawnpoint import T_Spawnpoint
 from pms.waypoint import T_Waypoint
 
+import logging
+
 
 class PmsReader(object):
   '''
     Parse a Soldat pms map file.
-    - 
+    -
     All of this code is my own, using the PMS map structure guide at the devs
     wiki as a reference for the structures.
   '''
@@ -41,13 +43,21 @@ class PmsReader(object):
 
       # Map file header
       self.header = T_Header()
-      h.readinto(self.header)
+      if not h.readinto(self.header):
+        logging.error('Failed reading header')
+        return
+
+      logging.info('read header. now reading polys')
 
       # All polygons
       for i in xrange(self.header.PolyCount):
         polygon = T_Polygon()
-        h.readinto(polygon)
+        if not h.readinto(polygon):
+          logging.error('Failed reading polygon #{}'.format(i))
+          return
         self.polygons.append(polygon)
+
+      logging.info('read polys now doing sector stuff')
 
       # Skip sector data we don't immediately care about
       sector_division = self._get_long(h)
@@ -62,40 +72,63 @@ class PmsReader(object):
       self.max_x = sector_division * num_sectors
       self.max_y = sector_division * num_sectors
 
+      logging.info('read sectors now reading props')
+
       # All props (scenery placements)
       prop_count = self._get_long(h)
       for i in xrange(prop_count):
         prop = T_Prop()
-        h.readinto(prop)
+        if not h.readinto(prop):
+          logging.error('Failed reading prop #{}'.format(i))
+          return
         self.props.append(prop)
+
+      logging.info('read props now reading sceneries')
 
       # All sceneries (map prop style to scenery filename)
       scenery_count = self._get_long(h)
       for i in xrange(scenery_count):
         scenery = T_Scenery()
-        h.readinto(scenery)
+        if not h.readinto(scenery):
+          logging.error('Failed reading scenery #{}'.format(i))
+          return
         self.sceneries[i + 1] = scenery
+
+      logging.info('read sceneries now reading colliders')
 
       # Colliders
       collider_count = self._get_long(h)
       for i in xrange(collider_count):
         collider = T_Collider()
-        h.readinto(collider)
+        if not h.readinto(collider):
+          logging.error('Failed reading collider #{}'.format(i))
+          return
         self.colliders.append(collider)
+
+      logging.info('read colliders now reading spawns')
 
       # Spawnpoints
       spawnpoint_count = self._get_long(h)
       for i in xrange(spawnpoint_count):
         spawnpoint = T_Spawnpoint()
-        h.readinto(spawnpoint)
+        if not h.readinto(spawnpoint):
+          logging.error('Failed reading spawnpoint #{}'.format(i))
+          return
         self.spawnpoints.append(spawnpoint)
+
+      logging.info('read spawns now reading waypoints')
 
       # Waypoints
       waypoint_count = self._get_long(h)
+
       for i in xrange(waypoint_count):
         waypoint = T_Waypoint()
-        h.readinto(waypoint)
+        if not h.readinto(waypoint):
+          logging.error('Failed reading waypoint #{}'.format(i))
+          return
         self.waypoints.append(waypoint)
+
+      logging.info('read waypoints')
 
   @property
   def name(self):
