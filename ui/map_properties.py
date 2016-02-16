8a147c127aa9d58b0b0a9df08dd96c84a1e61b24
Joe Gillotti <joe@u13.net>
2016-02-15 17:51:36 -0800

    Adding more items to the map properties box
    We now have (with values filled in):
    
    - Name
    - Steps (dropdown)
    - Weather (dropdown)
    - Jets (slider)
    - Grenades (spinbox)
    - Medkits (spinbox)

--- ui/map_properties.py
+++ ui/map_properties.py
@@ -1,17 +1,37 @@
 from PyQt4 import QtGui, QtCore, uic
+from pms.constants import T_Steps, T_Weather
 
 class MapPropertiesWindow(QtGui.QMainWindow):
 
   '''
-    Class corresponding to the main window. Try to delegate as much logic to
-    outside classes as possible and just keep this a high level shim tying
-    everything together
+    Small modal window which lets you change simple settings on the map
   '''
 
   def __init__(self, state):
+    '''
+      Localize state and populate drop downs with available values and
+      register callbacks
+    '''
     super(MapPropertiesWindow, self).__init__()
     self.ui = uic.loadUi('ui/map_properties.ui', self)
     self.state = state
+    self.ui.weatherComboBox.addItems(T_Weather)
+    self.ui.stepsComboBox.addItems(T_Steps)
+
+    self.ui.saveButton.clicked.connect(self.save_info)
 
   def update_info(self):
+    '''
+      Set text fields and drop down indexes to what the map currently has
+      configured
+    '''
     self.ui.mapNameBox.setText(self.state.pms_object.name)
+    self.ui.weatherComboBox.setCurrentIndex(self.state.pms_object.header.Weather)
+    self.ui.stepsComboBox.setCurrentIndex(self.state.pms_object.header.Steps)
+    self.ui.jetsSlider.setValue(self.state.pms_object.header.Jets)
+    self.ui.grenadesSpinBox.setValue(self.state.pms_object.header.Grenades)
+    self.ui.medkitsSpinBox.setValue(self.state.pms_object.header.Medkits)
+
+  def save_info(self):
+    print 'would save'
+    self.close()
